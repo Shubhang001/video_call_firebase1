ced3fa49ad6c6a4ebe85c5a02abd7b8b168bcc2c
Shubhang <shubhangshukla001@gmail.com>
2023-10-27 18:11:04 +0530

    Zego video call

---
+++ lib/constants.dart
@@ -0,0 +1,41 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+import 'package:video_call_firebase1/homepage.dart';
+import 'package:video_call_firebase1/login_screen.dart';
+
+// Project imports:
+import 'home_page.dart';
+import 'login_page.dart';
+
+class PageRouteNames {
+  static const String login = '/login';
+  static const String home = '/home_page';
+}
+
+const TextStyle textStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 13.0,
+  decoration: TextDecoration.none,
+);
+
+Map<String, WidgetBuilder> routes = {
+  PageRouteNames.login: (context) => LoginScreen(),
+  PageRouteNames.home: (context) => const MyHomePage(),
+};
+
+class UserInfo {
+  String id = '';
+  String name = '';
+
+  UserInfo({
+    required this.id,
+    required this.name,
+  });
+
+  bool get isEmpty => id.isEmpty;
+
+  UserInfo.empty();
+}
+
+UserInfo currentUser = UserInfo.empty();
+const String cacheUserIDKey = 'cache_user_id_key';
---
+++ lib/static.dart
@@ -0,0 +1,5 @@
+class Statics {
+  static String appSign =
+      '12c5c8d7d204b94d093489419388bc722d17b3160ba4ae1caaab1e8b7afe764e';
+  static int appID = 493944126;
+}
